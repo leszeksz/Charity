89cb169547f112508bcf1a16dca21c7e73e60cbf
leszeksz <leszek.szczotka@o2.pl>
2020-11-12 18:35:12 +0100

    HomeController implementations

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
 
 import java.util.List;
 
@@ -13,9 +15,11 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    InstitutionService institutionService;
     DonationService donationService;
 
-    public HomeController(DonationService donationService){
+    public HomeController(DonationService donationService, InstitutionService institutionService){
+        this.institutionService = institutionService;
         this.donationService = donationService;
     }
 
@@ -23,6 +27,12 @@ public class HomeController {
     public String homeAction(Model model){
 
         return "index";
+    }
+
+    @ModelAttribute
+    void institutionsList(Model model){
+        List<Institution> institutions = institutionService.findAll();
+        model.addAttribute("allInstitutions", institutions);
     }
 
     @ModelAttribute
--- src/main/java/pl/coderslab/charity/service/InstitutionServiceDb.java
+++ src/main/java/pl/coderslab/charity/service/InstitutionServiceDb.java
@@ -1,6 +1,18 @@
 package pl.coderslab.charity.service;
 
 import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
 
-public class InstitutionServiceDb extends Institution {
+import java.util.List;
+
+public class InstitutionServiceDb implements InstitutionService {
+    InstitutionRepository institutionRepository;
+
+    public InstitutionServiceDb(InstitutionRepository institutionRepository){
+        this.institutionRepository = institutionRepository;
+    }
+    @Override
+    public List<Institution> findAll() {
+        return institutionRepository.findAll();
+    }
 }
