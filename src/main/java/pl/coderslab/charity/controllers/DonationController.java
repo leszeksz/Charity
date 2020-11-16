e188e5c9934ad28b295d43a7e2926f818cf4e0f4
leszeksz <leszek.szczotka@o2.pl>
2020-11-16 11:35:59 +0100

    first part of form added

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -1,7 +1,15 @@
 package pl.coderslab.charity.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DonationController {
+
+    @RequestMapping("/donate")
+    public String donationAction(){
+        return "form";
+    }
+
+
 }
